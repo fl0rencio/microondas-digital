13b3d04dac861d47356b0c93b505140eea99ffa7
Wendel Florencio <wendel.github@gmail.com>
2023-06-11 22:12:22 -0300

    Criacao do projeto microondas digital

---
+++ Microondas/Microwave.cs
@@ -0,0 +1,94 @@
+﻿namespace Microondas
+{
+	internal class Microwave
+	{
+		public static int Tempo { get; set; }
+		public static int Potencia { get; set; }
+
+		public Microwave(int tempo, int potencia)
+		{
+			Tempo = tempo;
+			Potencia = potencia;
+		}
+
+		public static void ValidarTempo()
+		{
+			if (Tempo < 1 || Tempo > 120)
+			{
+				Console.WriteLine("O tempo deve ser entre 1 e 120.");
+				Console.WriteLine("Digite o tempo desejado:");
+			}
+		}
+
+		public static void ValidarPotencia()
+		{
+			if (Potencia == 0)
+			{
+				Potencia = 10;
+				Console.WriteLine(Potencia);
+			}
+			else if (Potencia < 1 || Potencia > 10)
+			{
+				Console.WriteLine("A potência deve ser de 1 a 10, caso não seja informada a potência será de 10.");
+				Console.WriteLine("Digite a potência desejada:");
+			}
+		}
+
+		public static void InicioRapido()
+		{
+			Tempo = 30;
+			Potencia = 10;
+			Console.WriteLine($"Tempo: {Tempo} | Potência: {Potencia}");
+			DefinirModoAquecimento(Tempo, Potencia);
+		}
+
+		public static void IniciarAquecimento()
+		{
+			DefinirModoAquecimento(Tempo, Potencia);
+		}
+
+		public static void Adicionar30Segundos()
+		{
+			DefinirModoAquecimento(30, Potencia);
+		}
+
+		private static void DefinirModoAquecimento(int tempo, int potencia)
+		{
+			var modoAquecimento = string.Empty;
+
+			for (int i = 1; i <= tempo; i++)
+			{
+				for (int j = 1; j <= potencia; j++)
+				{
+					modoAquecimento += ".";
+				}
+
+				if (potencia > 1)
+					modoAquecimento += " ";
+
+			}
+
+			if (potencia > 1)
+			{
+				var processamento = modoAquecimento.Split(" ");
+
+				foreach (var item in processamento)
+				{
+					Thread.Sleep(1000);
+					Console.Write($"{item} ");
+				}
+			}
+			else
+			{
+				for (int i = 1; i <= tempo; i++)
+				{
+					Thread.Sleep(1000);
+					Console.Write(".");
+				}
+			}
+
+			Console.Write(" Aquecimento concluído");
+			Console.WriteLine();
+		}
+	}
+}
---
+++ Microondas/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using Microondas;
+
+Console.WriteLine("Selecione a opção desejada:");
+Console.WriteLine("1- Início rápido");
+Console.WriteLine("2- Definir tempo e potência de aquecimento");
+
+int.TryParse(Console.ReadLine(), out int opcaoDesejada);
+if (opcaoDesejada == 1)
+{
+	Microwave.InicioRapido();
+}
+else if (opcaoDesejada == 2)
+{
+	Console.WriteLine("Digite o tempo desejado:");
+	int.TryParse(Console.ReadLine(), out int tempo);	
+
+	while (tempo < 1 || tempo > 120)
+	{
+		Microwave.ValidarTempo();
+		int.TryParse(Console.ReadLine(), out int tempoValido);
+		Microwave.Tempo = tempoValido;
+		Microwave.ValidarTempo();
+	}
+
+	Console.WriteLine();
+	Console.WriteLine("Digite a potência desejada:");
+	int.TryParse(Console.ReadLine(), out int potencia);	
+
+	while (potencia < 1 || potencia > 10)
+	{
+		Microwave.ValidarPotencia();
+		int.TryParse(Console.ReadLine(), out int potenciaValida);
+		Microwave.Potencia = potenciaValida;
+		Microwave.ValidarPotencia();
+	}
+	
+	new Microwave(tempo, potencia);
+
+	Console.WriteLine("Digite OK ou tecle ENTER para iniciar o aquecimento:");
+	string iniciarAquecimento = Console.ReadLine();
+	if (string.IsNullOrWhiteSpace(iniciarAquecimento) || iniciarAquecimento.Equals("ok", StringComparison.OrdinalIgnoreCase))
+	{
+		Microwave.IniciarAquecimento();
+	}
+}
+
+Console.WriteLine("3- Adiciodar 30 segundos no aquecimento");
+int.TryParse(Console.ReadLine(), out int adicionar30Seg);
+if (adicionar30Seg == 3)
+{
+	Microwave.Adicionar30Segundos();
+}
